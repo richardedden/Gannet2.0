53aa2640d0ce9b57ceedd66016d46997b07df2ac
nputs84 <nickputs@gmail.com>
2013-11-26 15:08:07 -0500

    Comment added re: needs for Gannet 1 matlab files
    %NP26-11-13 freadVAXG not found in Gannet2.0 folder. Still necessary to
    have original Gannet added to path

--- SDATreadMEGA.m
+++ SDATreadMEGA.m
@@ -12,7 +12,7 @@ A=zeros(da_xres,1);
  totalframes=1;
  totalpoints = totalframes*da_xres*da_yres*2;
 
-    raw_data = freadVAXG(fid, totalpoints, 'float');
+    raw_data = freadVAXG(fid, totalpoints, 'float'); %NP26-11-13 freadVAXG not found in Gannet2.0 folder. Still necessary to have original Gannet added to path
  
     TempData=reshape(raw_data,[2 da_xres da_yres]);
     A=squeeze(TempData(1,:,:)+1i*TempData(2,:,:));
