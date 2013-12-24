088710669ad298f8fb34d1db25bd07fb086c0b41
richardedden <raee2@jhu.edu>
2013-12-24 14:46:04 +0000

    GSH fitting incorporated into GannetFit
    New parameter MRS_struct.target (set in GannetPreinitialise) controls
    modelling approach. If 'GSH', the complicated GSH fitting is used.
    Integral ratio to Cr should be OK - not sure whether it'll work on
    batch input.

--- GannetPreInitialise.m
+++ GannetPreInitialise.m
@@ -13,6 +13,7 @@ function MRS_struct=GannetPreInitialise(MRS_struct)
     MRS_struct.TE=68; %This should be parsed from headers where possible
     MRS_struct.LarmorFreq=123.0; %This should be parsed from headers where possible
     %In general, LarmorFreq is 127.8 on Philips,
+    MRS_struct.target='GABA'; %Other option is GSH
     MRS_struct.ONOFForder='onfirst';
     %Options are MRS_struct.ONOFForder='onfirst' or 'offfirst';
     MRS_struct.Water_Positive=1;
