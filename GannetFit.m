c6340bbedffe961ac826e3ec138757e3c1241274
nputs84 <nickputs@gmail.com>
2014-01-27 15:49:31 -0500

    water freq index no longer set at 4.7
    80ms .data fell over with fitting water. Fit init parameters for water
    peak freq now calculated rather than set.

--- GannetFit.m
+++ GannetFit.m
@@ -204,7 +204,7 @@ for ii=1:numscans
     if strcmp(MRS_struct.p.Reference_compound,'H2O')
         T1=20;
         %estimate height and baseline from data
-        maxinWater=max(real(WaterData(:)));
+        [maxinWater maxWaterIndex]=max(real(WaterData(:))); %NPRE, maxWaterIndex created to accomodate issues with water fitting init
         waterbase = mean(real(WaterData(1:500))); % avg
 
         %Philips data do not phase well based on first point, so do a preliminary
@@ -271,7 +271,7 @@ for ii=1:numscans
                 nlinopts = statset(nlinopts, 'MaxIter', 1e5);
                 %This double fit doesn't seem to work too well with the GE
                 %data... dig a little deeper
-                LGPModelInit = [maxinWater 20 4.7 0 waterbase -50 0];
+                LGPModelInit = [maxinWater 20 freq(maxWaterIndex) 0 waterbase -50 0];
                 [LGPModelParam(ii,:),residw] = nlinfit(freq(freqbounds), real(WaterData(ii,freqbounds)),...
                     @(xdummy,ydummy)	LorentzGaussModelP(xdummy,ydummy),...
                     LGPModelInit, nlinopts);
