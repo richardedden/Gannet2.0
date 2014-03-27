9b25d0c9c5b4f3b1146485477c9550027f369451
richardedden <raee2@jhu.edu>
2014-03-27 12:19:15 +0100

    PaperPlot UPGRADE!

--- PaperPlot.m
+++ PaperPlot.m
@@ -1,11 +1,21 @@
 function PaperPlot(x,y,options)
-%PaperPlot(MRS_struct.spec.freq,MRS_struct.spec.freq,'k')
+%PaperPlot(MRS_struct) or PaperPlot(MRS_struct.spec.freq,MRS_struct.spec.diff,'k')
+
 %This plots a spectrum (or all spectra) from the input arguments.
 %For paper output, save as .eps format (matlab pdf isn't good).
 
-if nargin == 2
+switch nargin
+    case 1
+        z=abs(x.spec.freq-3.55);
+        lowerbound=find(min(z)==z);
+        z=abs(x.spec.freq-2.79);%2.75
+        upperbound=find(min(z)==z);
+        freqbounds=lowerbound:upperbound;
+        freq=x.spec.freq(1,freqbounds);
+        plot(x.spec.freq(1,:),x.spec.diff(1,:),'k',freq,GaussModel(x.out.GABAModelFit(1,:),freq),'r');
+    case 2
     plot(x,y)
-else
+    case 3
     plot(x,y,options)
 end   
 
