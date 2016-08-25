1916a36a0739ac4e36a1565cee2da97f9939dd8f
mgsaleh7 <mgsaleh7@gmail.com>
2016-08-25 14:39:02 -0400

    HERMES of GABAGlx and GSH
    HERMES of GABAGlx and GSH now added to GannetLoad

--- Gannetplotprepostalign.m
+++ Gannetplotprepostalign.m
@@ -7,13 +7,22 @@ function Gannetplotprepostalign(MRS_struct, specno)
 
 if MRS_struct.p.HERMES
         numspec = 4;
-        
-        %To determine the output depending on the type of acquistion used -- MGSaleh 2016
+    
+    
+    
         SpectraToPlot = [eval(['MRS_struct.spec.', sprintf('%s',MRS_struct.p.target2),'.diff']); ...
         eval(['MRS_struct.spec.', sprintf('%s',MRS_struct.p.target2),'.diff_noalign']); ...
         eval(['MRS_struct.spec.', sprintf('%s',MRS_struct.p.target),'.diff']); ...
         eval(['MRS_struct.spec.', sprintf('%s',MRS_struct.p.target),'.diff_noalign']);];
     
+        % Estimate baseline from between GABAGlx or Lac and GSH. The values might be changed depending on the future choice of metabolites -- MGSaleh
+        z=abs(MRS_struct.spec.freq-3.6);
+        Glx_right=find(min(z)==z);
+        z=abs(MRS_struct.spec.freq-3.3);
+        GABA_left=find(min(z)==z);
+        z=abs(MRS_struct.spec.freq-2.8);
+        GABA_right=find(min(z)==z);;
+        specbaseline = (mean(real(SpectraToPlot(1,Glx_right:GABA_left)),2));
     
     
     
@@ -25,6 +34,15 @@ else
         SpectraToPlot = [eval(['MRS_struct.spec.', sprintf('%s',MRS_struct.p.target),'.diff']); ...
         eval(['MRS_struct.spec.', sprintf('%s',MRS_struct.p.target),'.diff_noalign']);];
     
+        % Estimate baseline from between Glx and GABA
+        z=abs(MRS_struct.spec.freq-3.6);
+        Glx_right=find(min(z)==z);
+        z=abs(MRS_struct.spec.freq-3.3);
+        GABA_left=find(min(z)==z);
+        z=abs(MRS_struct.spec.freq-2.8);
+        GABA_right=find(min(z)==z);;
+        specbaseline = (mean(real(SpectraToPlot(1,Glx_right:GABA_left)),2));
+    
     
 end
 
@@ -32,14 +50,14 @@ end
 % SpectraToPlot = [MRS_struct.spec.diff(specno,:); MRS_struct.spec.diff_noalign(specno,:)];
 
 
-% Estimate baseline from between Glx and GABA
-z=abs(MRS_struct.spec.freq-3.6);
-Glx_right=find(min(z)==z);
-z=abs(MRS_struct.spec.freq-3.3);
-GABA_left=find(min(z)==z);
-z=abs(MRS_struct.spec.freq-2.8);
-GABA_right=find(min(z)==z);;
-specbaseline = (mean(real(SpectraToPlot(1,Glx_right:GABA_left)),2));
+% % Estimate baseline from between Glx and GABA
+% z=abs(MRS_struct.spec.freq-3.6);
+% Glx_right=find(min(z)==z);
+% z=abs(MRS_struct.spec.freq-3.3);
+% GABA_left=find(min(z)==z);
+% z=abs(MRS_struct.spec.freq-2.8);
+% GABA_right=find(min(z)==z);;
+% specbaseline = (mean(real(SpectraToPlot(1,Glx_right:GABA_left)),2));
 
 
 % averaged gaba height across all scans - to estimate stack spacing
@@ -48,6 +66,8 @@ gabaheight = mean(gabaheight);
 plotstackoffset = [ 0 : (numspec-1) ]';
 plotstackoffset = plotstackoffset * gabaheight;
 plotstackoffset = plotstackoffset - specbaseline;
+
+
 
 % Added by MGSaleh 2016
 if MRS_struct.p.HERMES
@@ -58,6 +78,8 @@ if MRS_struct.p.HERMES
     SpectraToPlot(3:4,:) = SpectraToPlot(3:4,:) + [max(shft,[],1); max(shft,[],1)] ;
     plot(MRS_struct.spec.freq, real(SpectraToPlot((3),:)),'b',MRS_struct.spec.freq, real(SpectraToPlot((4),:)),'r');
     hold off
+    
+   
     
 else
     SpectraToPlot = SpectraToPlot + ...
@@ -75,6 +97,9 @@ hl=legend(legendtxt);
 set(hl,'EdgeColor',[1 1 1]);
 set(gca,'XDir','reverse');
 oldaxis = axis;
+
+% yaxismax
+% yaxismin
 yaxismax = (numspec + 2) *gabaheight; % top spec + 2* height of gaba
 yaxismin =  - 2* gabaheight; % extend 2* gaba heights below zero
 if (yaxismax<yaxismin)
@@ -82,6 +107,6 @@ if (yaxismax<yaxismin)
     yaxismin=yaxismax;
     yaxismax=dummy;
 end
-% yaxismax
-% yaxismin
+
+
 axis([0 5  yaxismin yaxismax])
