f14e9c9d9e20744bbea46f6396c233faa9572ffc
markmikkelsen <mark.mikkelsen@gmail.com>
2017-08-08 11:32:01 -0400

    Added residual zero-order phase correction to Spectral_Registration_HERMES.m

--- Spectral_Registration_HERMES.m
+++ Spectral_Registration_HERMES.m
@@ -37,9 +37,11 @@ lb = [0 -Inf];
 ub = [Inf Inf];
 
 % Optimization options
+lsqopts = optimset('lsqcurvefit');
+lsqopts = optimset(lsqopts, 'MaxIter', 1e5, 'MaxFunEvals', 1e5, 'TolX', 1e-10, 'TolFun', 1e-10);
 nlinopts = statset('nlinfit');
-nlinopts = statset(nlinopts,'MaxIter',1e5);
-mleopts  = statset('MaxIter',1e5,'MaxFunEvals',1e5);
+nlinopts = statset(nlinopts, 'MaxIter', 1e5, 'MaxFunEvals', 1e5, 'TolX', 1e-10, 'TolFun', 1e-10);
+mleopts  = statset('MaxIter', 1e5, 'MaxFunEvals', 1e5, 'TolX', 1e-10, 'TolFun', 1e-10);
 
 % Set dimensions of figures of histograms
 if strcmpi(showPlots,'y')
@@ -64,7 +66,7 @@ while SpecRegLoop > -1
     flatdata(:,1,:) = real(DataToAlign(1:tMax,SubspecToAlign == SpecRegLoop));
     flatdata(:,2,:) = imag(DataToAlign(1:tMax,SubspecToAlign == SpecRegLoop));
     
-    % Select reference transient
+    % Reference transient
     flattarget = median(flatdata,3); % median across transients
     %flattarget = squeeze(flatdata(:,:,1)); % Mth transient
     target = flattarget(:);
@@ -112,8 +114,8 @@ while SpecRegLoop > -1
     
     if strcmpi(showPlots,'y')
         % Histogram of frequency offsets
-        H = figure(333);
-        set(H, 'Color', 'w', 'Units', 'Normalized', 'OuterPosition', [d.l d.b d.w d.h]);
+        H1 = figure(333);
+        set(H1, 'Color', 'w', 'Units', 'Normalized', 'OuterPosition', [d.l d.b d.w d.h]);
         subplot(1,2,1);
         bins = linspace(min(MRS_struct.out.MLalign.f.x(count,:,ii)), max(MRS_struct.out.MLalign.f.x(count,:,ii)), 15);
         binWidth = abs(bins(1)-bins(2));
@@ -139,7 +141,7 @@ while SpecRegLoop > -1
         set(gca, 'FontSize', 12, 'TickDir', 'out', 'Box', 'off');
         
         drawnow;
-        pause(1);
+        %pause(1);
     end
     
     corrloop_d = find(SubspecToAlign == SpecRegLoop);
@@ -163,52 +165,56 @@ while SpecRegLoop > -1
     
     if SpecRegLoop == 0
         
-%         MRS_struct.out.MLalign.f_aligned.x(ii,:) = CorrParsML(:,1);
-%         start = [std(MRS_struct.out.MLalign.f_aligned.x(ii,:))/2, median(MRS_struct.out.MLalign.f_aligned.x(ii,:))];
-%         MRS_struct.out.MLalign.f_aligned.p(ii,:) = ...
-%             mle(MRS_struct.out.MLalign.f_aligned.x(ii,:), 'pdf', Cauchy, 'start', start, 'lower', lb.c, 'upper', ub.c, 'options', mleopts);
-%         MRS_struct.out.MLalign.f_aligned.fx(ii,:) = ...
-%             linspace(1.1*min(MRS_struct.out.MLalign.f_aligned.x(ii,:)), 1.1*max(MRS_struct.out.MLalign.f_aligned.x(ii,:)), 1e3);
-%         MRS_struct.out.MLalign.f_aligned.pdf(ii,:) = ...
-%             Cauchy(MRS_struct.out.MLalign.f_aligned.fx(ii,:), MRS_struct.out.MLalign.f_aligned.p(ii,1), MRS_struct.out.MLalign.f_aligned.p(ii,2));
-%         
-%         MRS_struct.out.MLalign.ph_aligned.x(ii,:) = CorrParsML(:,2);
-%         start = [std(MRS_struct.out.MLalign.ph_aligned.x(ii,:))/2, median(MRS_struct.out.MLalign.ph_aligned.x(ii,:))];
-%         MRS_struct.out.MLalign.ph_aligned.p(ii,:) = ...
-%             mle(MRS_struct.out.MLalign.ph_aligned.x(ii,:), 'pdf', Cauchy, 'start', start, 'lower', lb.c, 'upper', ub.c, 'options', mleopts);
-%         MRS_struct.out.MLalign.ph_aligned.fx(ii,:) = ...
-%             linspace(1.1*min(MRS_struct.out.MLalign.ph_aligned.x(ii,:)), 1.1*max(MRS_struct.out.MLalign.ph_aligned.x(ii,:)), 1e3);
-%         MRS_struct.out.MLalign.ph_aligned.pdf(ii,:) = ...
-%             Cauchy(MRS_struct.out.MLalign.ph_aligned.fx(ii,:), MRS_struct.out.MLalign.ph_aligned.p(ii,1), MRS_struct.out.MLalign.ph_aligned.p(ii,2));
-%         
-%         clf(H);
-%         subplot(1,2,1);
-%         bins = linspace(min(MRS_struct.out.MLalign.f_aligned.x(ii,:)), max(MRS_struct.out.MLalign.f_aligned.x(ii,:)), 20);
-%         binWidth = abs(bins(1)-bins(2));
-%         h = bar(bins, histc(MRS_struct.out.MLalign.f_aligned.x(ii,:),bins)/(length(MRS_struct.out.MLalign.f_aligned.x(ii,:))*binWidth), 'histc');
-%         h.FaceColor = [0.8 0.8 0.8];
-%         hold on;
-%         plot(MRS_struct.out.MLalign.f_aligned.fx(ii,:), MRS_struct.out.MLalign.f_aligned.pdf(ii,:), 'Color', [1 0 0], 'LineWidth', 1.2);
-%         hold off;
-%         xlabel('\Deltaf (Hz)', 'FontSize', 15);
-%         ylabel('P(x)', 'FontSize', 15);
-%         set(gca, 'FontSize', 12, 'TickDir', 'out', 'Box', 'off');
-%         
-%         subplot(1,2,2);
-%         bins = linspace(min(MRS_struct.out.MLalign.ph_aligned.x(ii,:)), max(MRS_struct.out.MLalign.ph_aligned.x(ii,:)), 20);
-%         binWidth = abs(bins(1)-bins(2));
-%         h = bar(bins, histc(MRS_struct.out.MLalign.ph_aligned.x(ii,:),bins)/(length(MRS_struct.out.MLalign.ph_aligned.x(ii,:))*binWidth), 'histc');
-%         h.FaceColor = [0.8 0.8 0.8];
-%         hold on;
-%         plot(MRS_struct.out.MLalign.ph_aligned.fx(ii,:), MRS_struct.out.MLalign.ph_aligned.pdf(ii,:), 'Color', [1 0 0], 'LineWidth', 1.2);
-%         hold off;
-%         xlabel('\Delta\phi (deg)', 'FontSize', 15);
-%         ylabel('P(x)', 'FontSize', 15);
-%         set(gca, 'FontSize', 12, 'TickDir', 'out', 'Box', 'off');
-%         
-%         drawnow;
-%         %pause(1);        
-      
+        if strcmpi(showPlots,'y')
+            
+            MRS_struct.out.MLalign.f_aligned.x(ii,:) = CorrParsML(:,1);
+            start = [std(MRS_struct.out.MLalign.f_aligned.x(ii,:))/2, median(MRS_struct.out.MLalign.f_aligned.x(ii,:))];
+            MRS_struct.out.MLalign.f_aligned.p(ii,:) = ...
+                mle(MRS_struct.out.MLalign.f_aligned.x(ii,:), 'pdf', Cauchy, 'start', start, 'lower', lb, 'upper', ub, 'options', mleopts);
+            MRS_struct.out.MLalign.f_aligned.fx(ii,:) = ...
+                linspace(1.1*min(MRS_struct.out.MLalign.f_aligned.x(ii,:)), 1.1*max(MRS_struct.out.MLalign.f_aligned.x(ii,:)), 1e3);
+            MRS_struct.out.MLalign.f_aligned.pdf(ii,:) = ...
+                Cauchy(MRS_struct.out.MLalign.f_aligned.fx(ii,:), MRS_struct.out.MLalign.f_aligned.p(ii,1), MRS_struct.out.MLalign.f_aligned.p(ii,2));
+            
+            MRS_struct.out.MLalign.ph_aligned.x(ii,:) = CorrParsML(:,2);
+            start = [std(MRS_struct.out.MLalign.ph_aligned.x(ii,:))/2, median(MRS_struct.out.MLalign.ph_aligned.x(ii,:))];
+            MRS_struct.out.MLalign.ph_aligned.p(ii,:) = ...
+                mle(MRS_struct.out.MLalign.ph_aligned.x(ii,:), 'pdf', Cauchy, 'start', start, 'lower', lb, 'upper', ub, 'options', mleopts);
+            MRS_struct.out.MLalign.ph_aligned.fx(ii,:) = ...
+                linspace(1.1*min(MRS_struct.out.MLalign.ph_aligned.x(ii,:)), 1.1*max(MRS_struct.out.MLalign.ph_aligned.x(ii,:)), 1e3);
+            MRS_struct.out.MLalign.ph_aligned.pdf(ii,:) = ...
+                Cauchy(MRS_struct.out.MLalign.ph_aligned.fx(ii,:), MRS_struct.out.MLalign.ph_aligned.p(ii,1), MRS_struct.out.MLalign.ph_aligned.p(ii,2));
+            
+            clf(H1);
+            subplot(1,2,1);
+            bins = linspace(min(MRS_struct.out.MLalign.f_aligned.x(ii,:)), max(MRS_struct.out.MLalign.f_aligned.x(ii,:)), 20);
+            binWidth = abs(bins(1)-bins(2));
+            h = bar(bins, histc(MRS_struct.out.MLalign.f_aligned.x(ii,:),bins)/(length(MRS_struct.out.MLalign.f_aligned.x(ii,:))*binWidth), 'histc');
+            h.FaceColor = [0.8 0.8 0.8];
+            hold on;
+            plot(MRS_struct.out.MLalign.f_aligned.fx(ii,:), MRS_struct.out.MLalign.f_aligned.pdf(ii,:), 'Color', [1 0 0], 'LineWidth', 1.2);
+            hold off;
+            xlabel('\Deltaf (Hz)', 'FontSize', 15);
+            ylabel('P(x)', 'FontSize', 15);
+            set(gca, 'FontSize', 12, 'TickDir', 'out', 'Box', 'off');
+            
+            subplot(1,2,2);
+            bins = linspace(min(MRS_struct.out.MLalign.ph_aligned.x(ii,:)), max(MRS_struct.out.MLalign.ph_aligned.x(ii,:)), 20);
+            binWidth = abs(bins(1)-bins(2));
+            h = bar(bins, histc(MRS_struct.out.MLalign.ph_aligned.x(ii,:),bins)/(length(MRS_struct.out.MLalign.ph_aligned.x(ii,:))*binWidth), 'histc');
+            h.FaceColor = [0.8 0.8 0.8];
+            hold on;
+            plot(MRS_struct.out.MLalign.ph_aligned.fx(ii,:), MRS_struct.out.MLalign.ph_aligned.pdf(ii,:), 'Color', [1 0 0], 'LineWidth', 1.2);
+            hold off;
+            xlabel('\Delta\phi (deg)', 'FontSize', 15);
+            ylabel('P(x)', 'FontSize', 15);
+            set(gca, 'FontSize', 12, 'TickDir', 'out', 'Box', 'off');
+            
+            drawnow;
+            %pause(1);
+            
+        end
+        
         % Line-broadening, zero-filling and FFT
         FullData = DataToAlign .* repmat((exp(-time*MRS_struct.p.LB*pi)), [1 size(MRS_struct.fids.data,2)]);
         AllFramesFTrealign = fftshift(fft(FullData,MRS_struct.p.ZeroFillTo,1),1);
@@ -226,18 +232,62 @@ while SpecRegLoop > -1
         % Apply circular frequency shifts
         for corrloop = 1:size(AllFramesFTrealign,2)
             AllFramesFTrealign(:,corrloop) = circshift(AllFramesFTrealign(:,corrloop), CrFreqShift_pts(corrloop));
-        end        
+        end
+        
+        % Use ChoCr signals of SUM spectrum for final phasing
+        SUM = mean(AllFramesFTrealign,2);        
+        freqrange = MRS_struct.spec.freq >= 2.9 & MRS_struct.spec.freq <= 3.3;
+        freq = MRS_struct.spec.freq(freqrange);
+        
+        SUM_ChoCr = SUM(freqrange);
+        Baseline_offset = real(SUM_ChoCr(1)+SUM_ChoCr(end))/2;
+        Width_estimate = 0.05;
+        Area_estimate = -(max(real(SUM_ChoCr))-min(real(SUM_ChoCr))) * Width_estimate * 4;
+        
+        LorentzModelInit = [Area_estimate Area_estimate Width_estimate Width_estimate 3.02 3.20 pi Baseline_offset 1 1];
+        lb = [Area_estimate/10 Area_estimate/10 Width_estimate/10 Width_estimate/10 3.02-0.02 3.20-0.02 -pi 2e3*Area_estimate 2e3*Area_estimate 2e3*Area_estimate];
+        ub = [-Area_estimate*10 -Area_estimate*10 Width_estimate*10 Width_estimate*10 3.02+0.02 3.20+0.02 pi -1e3*Area_estimate -1e3*Area_estimate -1e3*Area_estimate];
+        
+        LorentzModelInit = lsqcurvefit(@TwoLorentzModel, LorentzModelInit, freq, real(SUM_ChoCr)', lb, ub, lsqopts);
+        LorentzModelParam = nlinfit(freq, real(SUM_ChoCr)', @TwoLorentzModel, LorentzModelInit, nlinopts);
+        
+        if strcmpi(showPlots,'y')
+            H2 = figure(334);
+            hold on;
+            plot(freq', real(SUM_ChoCr), 'k');
+            plot(freq', TwoLorentzModel(LorentzModelParam,freq), 'r');
+            hold off;
+            set(gca,'xdir','reverse');
+            drawnow;
+            pause(1);
+        end
+        
+        % Convert to complex number then recalculate phase within 2*pi range
+        phi = LorentzModelParam(7);
+        phi = cos(phi) + 1i*sin(phi);
+        phi = angle(phi);
+        % Then fix to be within -pi...pi
+        offsetpos = pi*lt(phi,-pi/2);
+        offsetneg = -pi*gt(phi,pi/2);
+        phi = phi + offsetpos + offsetneg;
+        if phi > 0
+            phi = -phi;
+        end
+        
+        % Apply zero-order phase correction
+        AllFramesFTrealign = AllFramesFTrealign * exp(1i*phi);        
         
         % Reject transients that are greater than +/-3 st. devs. of MSEs
-        rejectFrames = zMSE > 3 | zMSE < -3;
-        if MRS_struct.p.HERMES
-            rejectFrames = reshape(rejectFrames, [4 size(MRS_struct.fids.data,2)/4])';
-        else
-            rejectFrames = reshape(rejectFrames, [2 size(MRS_struct.fids.data,2)/2])';
-        end
-        ind = any(rejectFrames,2);
-        rejectFrames(ind,:) = 1;
-        MRS_struct.out.reject(:,ii) = reshape(rejectFrames', [1 size(MRS_struct.fids.data,2)]);
+        MRS_struct.out.reject(:,ii) = zMSE > 3 | zMSE < -3;
+        %rejectFrames = zMSE > 3 | zMSE < -3;
+        %if MRS_struct.p.HERMES
+        %    rejectFrames = reshape(rejectFrames, [4 size(MRS_struct.fids.data,2)/4])';
+        %else
+        %    rejectFrames = reshape(rejectFrames, [2 size(MRS_struct.fids.data,2)/2])';
+        %end
+        %ind = any(rejectFrames,2);
+        %rejectFrames(ind,:) = 1;
+        %MRS_struct.out.reject(:,ii) = reshape(rejectFrames', [1 size(MRS_struct.fids.data,2)]);
         
     end
     
@@ -245,10 +295,38 @@ while SpecRegLoop > -1
     
 end
 
-if exist('H','var')
-    close(H);
+if exist('H1','var')
+    close(H1);
+    close(H2);
 end
 fprintf('\n');
+
+end
+
+
+function Lorentz = TwoLorentzModel(x, freq)
+
+area1 = x(1);
+area2 = x(2);
+hwhm1 = x(3);
+hwhm2 = x(4);
+f1 = x(5);
+f2 = x(6);
+phase = x(7);
+baseline1 = x(8);
+baseline2 = x(9);
+baseline3 = x(10);
+
+Absorption = 1./(2.*pi) .* area1 .* hwhm1 ./ ((freq-f1).^2 + hwhm1.^2) + ...
+             1./(2.*pi) .* area2 .* hwhm2 ./ ((freq-f2).^2 + hwhm2.^2);
+
+Dispersion = 1./(2*pi) .* area1 .* (freq-f1) ./ ((freq-f1).^2 + hwhm1.^2) + ...
+             1./(2*pi) .* area2 .* (freq-f2) ./ ((freq-f2).^2 + hwhm2.^2);
+
+Lorentz = cos(phase) .* Absorption + sin(phase) .* Dispersion + ...
+	  baseline1 .* (freq-f1) + ...
+      baseline2 .* sin(pi.*freq./1.31./4) + ...
+      baseline3 .* cos(pi.*freq./1.31./4);
 
 end
 
