68e59d68a1a936ab8af7f36b4e18cf4bebe7a5d0
markmikkelsen <mark.mikkelsen@gmail.com>
2017-11-28 11:51:37 -0500

    Avoid back/forward slash issues on Windows/Mac platforms

--- CallSPM12segmentation.m
+++ CallSPM12segmentation.m
@@ -2,32 +2,33 @@ function CallSPM12segmentation(anatimage)
 
 % Get file handling for spm sorted out
 spmhome = fileparts(which('spm'));
+tpm = cellstr(spm_select('ExtFPList',fullfile(spmhome,'tpm'),'TPM.nii'));
 
 matlabbatch{1}.spm.spatial.preproc.channel.vols = {[anatimage ',1']};
 matlabbatch{1}.spm.spatial.preproc.channel.biasreg = 0.001;
 matlabbatch{1}.spm.spatial.preproc.channel.biasfwhm = 60;
 matlabbatch{1}.spm.spatial.preproc.channel.write = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = {[spmhome '/tpm/TPM.nii,1']};
+matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = tpm(1);
 matlabbatch{1}.spm.spatial.preproc.tissue(1).ngaus = 2;
 matlabbatch{1}.spm.spatial.preproc.tissue(1).native = [1 0];
 matlabbatch{1}.spm.spatial.preproc.tissue(1).warped = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = {[spmhome '/tpm/TPM.nii,2']};
+matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = tpm(2);
 matlabbatch{1}.spm.spatial.preproc.tissue(2).ngaus = 2;
 matlabbatch{1}.spm.spatial.preproc.tissue(2).native = [1 0];
 matlabbatch{1}.spm.spatial.preproc.tissue(2).warped = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = {[spmhome '/tpm/TPM.nii,3']};
+matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = tpm(3);
 matlabbatch{1}.spm.spatial.preproc.tissue(3).ngaus = 2;
 matlabbatch{1}.spm.spatial.preproc.tissue(3).native = [1 0];
 matlabbatch{1}.spm.spatial.preproc.tissue(3).warped = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = {[spmhome '/tpm/TPM.nii,4']};
+matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = tpm(4);
 matlabbatch{1}.spm.spatial.preproc.tissue(4).ngaus = 3;
 matlabbatch{1}.spm.spatial.preproc.tissue(4).native = [0 0];
 matlabbatch{1}.spm.spatial.preproc.tissue(4).warped = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = {[spmhome '/tpm/TPM.nii,5']};
+matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = tpm(5);
 matlabbatch{1}.spm.spatial.preproc.tissue(5).ngaus = 4;
 matlabbatch{1}.spm.spatial.preproc.tissue(5).native = [0 0];
 matlabbatch{1}.spm.spatial.preproc.tissue(5).warped = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = {[spmhome '/tpm/TPM.nii,6']};
+matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = tpm(6);
 matlabbatch{1}.spm.spatial.preproc.tissue(6).ngaus = 2;
 matlabbatch{1}.spm.spatial.preproc.tissue(6).native = [0 0];
 matlabbatch{1}.spm.spatial.preproc.tissue(6).warped = [0 0];
--- CallSPM8segmentation.m
+++ CallSPM8segmentation.m
@@ -2,6 +2,7 @@ function CallSPM8segmentation(anatimage)
 
 % Get file handling for spm sorted out
 spmhome = fileparts(which('spm'));
+tpm = cellstr(spm_select('ExtFPList',fullfile(spmhome,'toolbox','Seg'),'TPM.nii'));
 
 matlabbatch{1}.spm.spatial.preproc.data = {[anatimage ',1']};
 matlabbatch{1}.spm.spatial.preproc.output.GM = [0 0 1];
@@ -9,9 +10,9 @@ matlabbatch{1}.spm.spatial.preproc.output.WM = [0 0 1];
 matlabbatch{1}.spm.spatial.preproc.output.CSF = [0 0 1];
 matlabbatch{1}.spm.spatial.preproc.output.biascor = 1;
 matlabbatch{1}.spm.spatial.preproc.output.cleanup = 0;
-matlabbatch{1}.spm.spatial.preproc.opts.tpm = {[spmhome '/tpm/grey.nii']
-                                               [spmhome '/tpm/white.nii']
-                                               [spmhome '/tpm/csf.nii']};
+matlabbatch{1}.spm.spatial.preproc.opts.tpm = {fullfile(spmhome,'tpm','grey.nii')
+                                               fullfile(spmhome,'tpm','white.nii')
+                                               fullfile(spmhome,'tpm','csf.nii')};
 matlabbatch{1}.spm.spatial.preproc.opts.ngaus = [2 2 2 4];
 matlabbatch{1}.spm.spatial.preproc.opts.regtype = 'mni';
 matlabbatch{1}.spm.spatial.preproc.opts.warpreg = 1;
@@ -25,27 +26,27 @@ matlabbatch{2}.spm.tools.preproc8.channel.vols = {[anatimage ',1']};
 matlabbatch{2}.spm.tools.preproc8.channel.biasreg = 0.0001;
 matlabbatch{2}.spm.tools.preproc8.channel.biasfwhm = 60;
 matlabbatch{2}.spm.tools.preproc8.channel.write = [0 0];
-matlabbatch{2}.spm.tools.preproc8.tissue(1).tpm = {[spmhome '/toolbox/Seg/TPM.nii,1']};
+matlabbatch{2}.spm.tools.preproc8.tissue(1).tpm = tpm(1);
 matlabbatch{2}.spm.tools.preproc8.tissue(1).ngaus = 2;
 matlabbatch{2}.spm.tools.preproc8.tissue(1).native = [1 0];
 matlabbatch{2}.spm.tools.preproc8.tissue(1).warped = [0 0];
-matlabbatch{2}.spm.tools.preproc8.tissue(2).tpm = {[spmhome '/toolbox/Seg/TPM.nii,2']};
+matlabbatch{2}.spm.tools.preproc8.tissue(2).tpm = tpm(2);
 matlabbatch{2}.spm.tools.preproc8.tissue(2).ngaus = 2;
 matlabbatch{2}.spm.tools.preproc8.tissue(2).native = [1 0];
 matlabbatch{2}.spm.tools.preproc8.tissue(2).warped = [0 0];
-matlabbatch{2}.spm.tools.preproc8.tissue(3).tpm = {[spmhome '/toolbox/Seg/TPM.nii,3']};
+matlabbatch{2}.spm.tools.preproc8.tissue(3).tpm = tpm(3);
 matlabbatch{2}.spm.tools.preproc8.tissue(3).ngaus = 2;
 matlabbatch{2}.spm.tools.preproc8.tissue(3).native = [1 0];
 matlabbatch{2}.spm.tools.preproc8.tissue(3).warped = [0 0];
-matlabbatch{2}.spm.tools.preproc8.tissue(4).tpm = {[spmhome '/toolbox/Seg/TPM.nii,4']};
+matlabbatch{2}.spm.tools.preproc8.tissue(4).tpm = tpm(4);
 matlabbatch{2}.spm.tools.preproc8.tissue(4).ngaus = 3;
 matlabbatch{2}.spm.tools.preproc8.tissue(4).native = [0 0];
 matlabbatch{2}.spm.tools.preproc8.tissue(4).warped = [0 0];
-matlabbatch{2}.spm.tools.preproc8.tissue(5).tpm = {[spmhome '/toolbox/Seg/TPM.nii,5']};
+matlabbatch{2}.spm.tools.preproc8.tissue(5).tpm = tpm(5);
 matlabbatch{2}.spm.tools.preproc8.tissue(5).ngaus = 4;
 matlabbatch{2}.spm.tools.preproc8.tissue(5).native = [0 0];
 matlabbatch{2}.spm.tools.preproc8.tissue(5).warped = [0 0];
-matlabbatch{2}.spm.tools.preproc8.tissue(6).tpm = {[spmhome '/toolbox/Seg/TPM.nii,6']};
+matlabbatch{2}.spm.tools.preproc8.tissue(6).tpm = tpm(6);
 matlabbatch{2}.spm.tools.preproc8.tissue(6).ngaus = 2;
 matlabbatch{2}.spm.tools.preproc8.tissue(6).native = [0 0];
 matlabbatch{2}.spm.tools.preproc8.tissue(6).warped = [0 0];
