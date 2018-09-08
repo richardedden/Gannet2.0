8f50d03cadd4479c8c8a358093db22ce8ee7bcaf
markmikkelsen <mark.mikkelsen@gmail.com>
2018-09-08 13:32:28 -0400

    Updated GEDeIdentify.m

--- GEDeIdentify.m
+++ GEDeIdentify.m
@@ -33,6 +33,7 @@ function GEDeIdentify(fnames)
 %
 %   Version history:
 %       2016-08-09: + Function created
+%       2018-09-05: + Smarter parsing of P-file header
 
 
 if nargin < 1 % De-identify all P-files in current directory
@@ -88,8 +89,7 @@ for ii = 1:length(fnames)
     end
     
     % Determine P-file version
-    [pfile_fid, pfile_fid_noID, pfile_hdr_sz, pfile_exam_offset, pfile_series_offset] = ...
-        VersionCheck(fnames{ii}, pfile_fid, pfile_fid_noID);
+    [pfile_fid, pfile_fid_noID, hdr] = VersionCheck(fnames{ii}, pfile_fid, pfile_fid_noID);
     
     % Determine how big the P-file is
     file_info = dir(fnames{ii});
@@ -99,33 +99,23 @@ for ii = 1:length(fnames)
     frewind(pfile_fid);
     pfile = fread(pfile_fid, pfile_sz, 'integer*2');
     
-    % Read some header information
-    frewind(pfile_fid);
-    hdr_val = fread(pfile_fid, 102, 'integer*2');
-    n_echoes = hdr_val(36);
-    point_sz = hdr_val(42);
-    n_points = hdr_val(52);
-    n_rows = hdr_val(53);
-    n_receivers = (hdr_val(102) - hdr_val(101)) + 1;
-            
     % Spectro prescan P-files
-    if n_points == 1 && n_rows == 1
-        n_points = 2048;
+    if hdr.n_points == 1 && hdr.n_rows == 1
+        hdr.n_points = 2048;
     end
     
     % Compute size (in bytes) of raw data
-    data_elements = n_points * 2;
-    my_frame = 1;    
-    total_frames = (n_rows - my_frame + 1) * n_echoes;
-    data_elements = data_elements * total_frames * n_receivers;
+    data_elements = hdr.n_points * 2;
+    total_frames = hdr.n_rows * hdr.n_echoes;
+    data_elements = data_elements * total_frames * hdr.n_rcvrs;
     
     % Read the raw data (using the appropriate precision)
-    fseek(pfile_fid, pfile_hdr_sz, 'bof');
-    if point_sz == 2
+    fseek(pfile_fid, hdr.hdr_sz, 'bof');
+    if hdr.point_sz == 2
         pfile_raw_data = fread(pfile_fid, data_elements, 'integer*2');
     else
         pfile_raw_data = fread(pfile_fid, data_elements, 'integer*4');
-    end    
+    end
     
     % Write all P-file bytes into new P-file
     frewind(pfile_fid_noID);
@@ -133,13 +123,13 @@ for ii = 1:length(fnames)
     
     % Overwrite EXAMDATATYPE structure that contains participant/exam
     % information with zeros in new P-file
-    exam_data_type_sz = pfile_series_offset - pfile_exam_offset;
-    fseek(pfile_fid_noID, pfile_exam_offset, 'bof');
+    exam_data_type_sz = hdr.series_offset - hdr.exam_offset;
+    fseek(pfile_fid_noID, hdr.exam_offset, 'bof');
     fwrite(pfile_fid_noID, zeros(exam_data_type_sz,1), 'integer*2');
     
     % Write raw data to new P-file (using the appropriate precision)
-    fseek(pfile_fid_noID, pfile_hdr_sz, 'bof');
-    if point_sz == 2
+    fseek(pfile_fid_noID, hdr.hdr_sz, 'bof');
+    if hdr.point_sz == 2
         fwrite(pfile_fid_noID, pfile_raw_data, 'integer*2');
     else
         fwrite(pfile_fid_noID, pfile_raw_data, 'integer*4');
@@ -159,7 +149,7 @@ exitFunc = 0;
 
 if nArgs < 1
     
-    if any(~cellfun('isempty', strfind(fnames, '_noID')))
+    if any(~cellfun('isempty', strfind(fnames, '_noID'))) %#ok<*STRCL1>
         resp = input('\nDe-identified files found in the directory! Proceed and overwrite? [y/n]: ','s');
         if strcmpi(resp, 'y')
             disp('Overwriting...');
@@ -170,8 +160,8 @@ if nArgs < 1
         end
         
         ind1 = strfind(fnames, '_noID');
-        ind2 = find(~cellfun('isempty', ind1));
-        fnames(ind2) = []; %#ok<FNDSB>
+        ind2 = ~cellfun('isempty', ind1);
+        fnames(ind2) = [];
     end
     
 else
@@ -195,27 +185,26 @@ else
 end
 
 
-function [pfile_fid, pfile_fid_noID, pfile_hdr_sz, pfile_exam_offset, pfile_series_offset] = ...
-    VersionCheck(fnames, pfile_fid, pfile_fid_noID)
+function [pfile_fid, pfile_fid_noID, hdr] = VersionCheck(fnames, pfile_fid, pfile_fid_noID)
 % Determine P-file version
 
 frewind(pfile_fid);
 rdbm_rev_num = fread(pfile_fid, 1, 'real*4');
 
 if rdbm_rev_num == 7.0 % Signa 8.X (LX)
-    pfile_hdr_sz = 39984;
-    pfile_exam_offset = pfile_hdr_sz - 1040 - 1028 - 1044;
-    pfile_series_offset = pfile_hdr_sz - 1028 - 1044;
+    hdr.hdr_sz = 39984;
+    hdr.exam_offset = hdr.hdr_sz - 1040 - 1028 - 1044;
+    hdr.series_offset = hdr.hdr_sz - 1028 - 1044;
 elseif rdbm_rev_num == 8.0 % Cardiac / MGD
-%     pfile_hdr_sz = 60464;
+    %hdr.hdr_sz = 60464;
     fclose(pfile_fid);
     fclose(pfile_fid_noID);
-    error('GEDeIdentify does not yet support P-file header revision: %f', rdbm_rev_num);
+    error('GEDeIdentify does not yet support P-file header revision: %g', rdbm_rev_num);
 elseif rdbm_rev_num > 5.0 && rdbm_rev_num < 6.0 % Signa 5.5
-%     pfile_hdr_sz = 39940;
+    %hdr.hdr_sz = 39940;
     fclose(pfile_fid);
     fclose(pfile_fid_noID);
-    error('GEDeIdentify does not yet support P-file header revision: %f', rdbm_rev_num);
+    error('GEDeIdentify does not yet support P-file header revision: %g', rdbm_rev_num);
 else
     % In 11.0 and later, the header and data are stored as little-endian
     fclose(pfile_fid);
@@ -226,23 +215,70 @@ else
     frewind(pfile_fid);
     rdbm_rev_num = fread(pfile_fid, 1, 'real*4');
     
-    if rdbm_rev_num == 9.0  % Excite2 11.0
-        pfile_hdr_sz = 61464;
-        pfile_exam_offset = pfile_hdr_sz - 1040 - 1536 - 1536;
-        pfile_series_offset = pfile_hdr_sz - 1536 - 1536;
-    elseif rdbm_rev_num >= 11.0 && rdbm_rev_num < 100.0  % 12.0 and later
+    if rdbm_rev_num == 9.0  % Excite2 11.0 product release
+        hdr.hdr_sz = 61464;
+        hdr.exam_offset = hdr.hdr_sz - 1040 - 1536 - 1536;
+        hdr.series_offset = hdr.hdr_sz - 1536 - 1536;
+    elseif rdbm_rev_num == 11.0  % 12.0 product release
         fseek(pfile_fid, 1468, 'bof');
-        pfile_hdr_sz = fread(pfile_fid, 1, 'integer*4');
+        hdr.hdr_sz = fread(pfile_fid, 1, 'integer*4');
         fseek(pfile_fid, 1496, 'bof');
-        pfile_exam_offset = fread(pfile_fid, 1, 'integer*4');
+        hdr.exam_offset = fread(pfile_fid, 1, 'integer*4');
         fseek(pfile_fid, 1500, 'bof');
-        pfile_series_offset = fread(pfile_fid, 1, 'integer*4');
-    else
-        fclose(pfile_fid);
-        fclose(pfile_fid_noID);
-        error('Invalid P-file header revision: %f', rdbm_rev_num);
+        hdr.series_offset = fread(pfile_fid, 1, 'integer*4');
+    elseif rdbm_rev_num > 11.0
+        chkRev = {'14.3','16','24','26.002'};
+        if ~any(strcmp(num2str(rdbm_rev_num), chkRev))
+            fclose(pfile_fid);
+            fclose(pfile_fid_noID);
+            error('GEDeIdentify does not yet support P-file header revision: %g', rdbm_rev_num);
+        end
+        
+        switch num2str(rdbm_rev_num)
+            case {'14.3','16','24'}
+                rdb_hdr_off_image     = 377;
+                rdb_hdr_off_data      = 368;
+                rdb_hdr_off_exam      = 375;
+                rdb_hdr_off_series    = 376;
+                rdb_hdr_ps_mps_freq   = 107;
+                rdb_hdr_nechoes       = 36;
+                rdb_hdr_point_size    = 42;
+                rdb_hdr_da_xres       = 52;
+                rdb_hdr_da_yres       = 53;
+                rdb_hdr_dab_start_rcv = 101;
+                rdb_hdr_dab_stop_rcv  = 102;
+            case '26.002'
+                rdb_hdr_off_image     = 11;
+                rdb_hdr_off_data      = 2;
+                rdb_hdr_off_exam      = 9;
+                rdb_hdr_off_series    = 10;
+                rdb_hdr_ps_mps_freq   = 123;
+                rdb_hdr_nechoes       = 74;
+                rdb_hdr_point_size    = 80;
+                rdb_hdr_da_xres       = 90;
+                rdb_hdr_da_yres       = 91;
+                rdb_hdr_dab_start_rcv = 133;
+                rdb_hdr_dab_stop_rcv  = 134;
+        end
+        
+        frewind(pfile_fid);
+        i_hdr_value = fread(pfile_fid, max(rdb_hdr_off_image, rdb_hdr_ps_mps_freq), 'integer*4');
+        frewind(pfile_fid);
+        hdr_value = fread(pfile_fid, rdb_hdr_dab_stop_rcv, 'integer*2');
+        
+        hdr.hdr_sz = i_hdr_value(rdb_hdr_off_data);
+        hdr.exam_offset = i_hdr_value(rdb_hdr_off_exam);
+        hdr.series_offset = i_hdr_value(rdb_hdr_off_series);
+        
+        hdr.n_echoes = hdr_value(rdb_hdr_nechoes);
+        hdr.point_sz = hdr_value(rdb_hdr_point_size);
+        hdr.n_points = hdr_value(rdb_hdr_da_xres);
+        hdr.n_rows = hdr_value(rdb_hdr_da_yres);
+        start_recv = hdr_value(rdb_hdr_dab_start_rcv);
+        stop_recv = hdr_value(rdb_hdr_dab_stop_rcv);
+        hdr.n_rcvrs = (stop_recv - start_recv) + 1;
     end
-    
 end
+
 
 
